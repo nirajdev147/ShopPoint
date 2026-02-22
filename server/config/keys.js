04ef3ddcb48c154ae48b5c9f2a3fea27753edb03
Niraj Dev <104443992+nirajdev147@users.noreply.github.com>
2026-02-22 12:54:48 +0545

    Refactor JWT_SECRET to use environment variable
    Change JWT_SECRET to use environment variable and add error handling.

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,3 +1,7 @@
-module.exports = {
-  JWT_SECRET: "mySuperSecretKey123!@#",
-};
+const JWT_SECRET = process.env.JWT_SECRET;
+
+if (!JWT_SECRET) {
+  throw new Error("❌ JWT_SECRET is not defined in environment variables");
+}
+
+module.exports = JWT_SECRET;
